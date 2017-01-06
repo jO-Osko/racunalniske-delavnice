1bca823d9d34a5c56a27ef659cd10b97bca11dd1
Filip <koprivec.filip@gmail.com>
2017-01-06 20:00:03 +0100

    Added untyped Dijkstra

---
+++ DijkstraUntyped/Graph.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+class Graph:
+    def __init__(self, vertexes):
+        if isinstance(vertexes, list):
+            self.vertexes = vertexes
+        else:
+            self.vertexes = list(vertexes)
+
+
+class Vertex:
+    def __init__(self, name: str, edges=None):
+        self.name = name
+        self.edges = edges or []
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def __repr__(self):
+        return self.name
+
+
+class Edge:
+    def __init__(self, weight, start, end):
+        self.weight = weight
+        self.start = start
+        self.end = end
